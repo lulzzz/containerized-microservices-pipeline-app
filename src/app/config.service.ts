cca403c56eef419c78db47a6ccef3221f4342357
Elizabeth Halper <elhalper@microsoft.com>
2018-05-10 11:42:17 -0700

    void return type for put request

--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -52,12 +52,12 @@ export class ConfigService {
     return header;
   }
 
-  public postChangeEmail(email: string, token: string): Observable<HttpResponse<LoginResponse>> {
+  public postChangeEmail(email: string, token: string): Observable<HttpResponse<void>> {
     const newemail: ChangeEmail = {
       email: email
     };
     const header = this.createHeader(token);
-    return this.http.put<HttpResponse<LoginResponse>>(this.configUrl + 'api/account', newemail, header);
+    return this.http.put<HttpResponse<void>>(this.configUrl + 'api/account', newemail, header);
   }
 
   public postCreate(username: string, email: string, password1: string): Observable<HttpResponse<LoginResponse>> {
